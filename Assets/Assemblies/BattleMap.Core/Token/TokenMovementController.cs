e598a17fe1b916b5d4941b94143d0b5781f6d733
Tom Wolfe <trwolfe13@gmail.com>
2021-08-16 13:57:31 +0100

    feature: Dragging a token now happens relative to the mouse's position over the token instead of centering the token on the mouse.

--- Assets/Assemblies/BattleMap.Core/Token/TokenMovementController.cs
+++ Assets/Assemblies/BattleMap.Core/Token/TokenMovementController.cs
@@ -4,12 +4,24 @@ namespace BattleMap.Core.Token
 {
     public class TokenMovementController : MonoBehaviour
     {
+        private Vector3 _dragDelta;
+
+        public void OnMouseDown()
+        {
+            var camera = UnityEngine.Camera.main;
+            var startPosition = camera.ScreenToWorldPoint(Input.mousePosition);
+            _dragDelta = transform.position - startPosition;
+        }
+
         public void OnMouseDrag()
         {
             var camera = UnityEngine.Camera.main;
-            var target = camera.ScreenToWorldPoint(Input.mousePosition);
-            target.z = transform.position.z;
-            transform.position = target;
+
+            var mouseCurrent = camera.ScreenToWorldPoint(Input.mousePosition);
+            var tokenCurrent = transform.position;
+            var tokenTarget = mouseCurrent + _dragDelta;
+            tokenTarget.z = tokenCurrent.z;
+            transform.position = tokenTarget;
         }
     }
 }
